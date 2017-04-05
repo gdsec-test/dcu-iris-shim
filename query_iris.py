7bf40058773a44a4aacf51acaa8e55bec01ce6b7
Chris Hauser <chauser@godaddy.com>
2017-04-05 09:56:03 -0700

    Added batch and run changes

--- query_iris.py
+++ query_iris.py
@@ -25,18 +25,19 @@ class QueryIris:
                 for row in data:
                     yield row
 
-    def get_phishing_and_malware(self, last_x_hours):
+    def get_phishing_and_malware(self, x_hours, less_than=False):
         cnxn = pyodbc.connect(self._dbstring)
         cnxn.autocommit = True
         cnxn.timeout = 0
         cursor = cnxn.cursor()
         incidents = []
+        logical = '<' if less_than else '>'
         try:
             query = "select iris_incidentID, iris_serviceID, OriginalEmailAddress " \
                     "from IRISIncidentMain where iris_groupID = '443' And (iris_serviceID = '226' " \
                     "Or iris_serviceID = '236') and (SPAM = 'False') and iris_statusID = 1 "
-            if last_x_hours > 0:
-                    query += "and CreateDate > \'%s\'" % (datetime.utcnow() - timedelta(hours=last_x_hours)).strftime("%Y-%m-%d %H:%M:%S")
+            if x_hours > 0:
+                    query += "and CreateDate " + logical + " \'%s\'" % (datetime.utcnow() - timedelta(hours=x_hours)).strftime("%Y-%m-%d %H:%M:%S")
             cursor.execute(query)  # DB query goes here
             incidents = QueryIris.rows(cursor)
         except Exception as e:
