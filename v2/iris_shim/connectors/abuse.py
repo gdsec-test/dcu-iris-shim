5c1674c796be736da2cc2a65ba62d64bf74817dc
Aaron Bean <abean@godaddy.com>
2018-10-31 08:41:00 -0700

    [CMAPT-2621] Update Shim v2 API connection (#21)

--- v2/iris_shim/connectors/abuse.py
+++ v2/iris_shim/connectors/abuse.py
@@ -1,18 +1,24 @@
 import logging
-
 import requests
 
 
 class AbuseAPI:
-    headers = {'Authorization': None}
 
-    def __init__(self, abuse_api_url):
+    def __init__(self, abuse_api_url, sso_key, sso_secret):
         self._logger = logging.getLogger(__name__)
         self._url = abuse_api_url
+        self._headers = {'Content-Type': 'application/json', 'Authorization': 'sso-key ' + sso_key + ':' + sso_secret}
 
     def post(self, type, source, report_id, reporter_email, create_date):
-        if not self.headers.get('Authorization'):
-            self.headers['Authorization'] = self._get_jwt()
+        """
+        Connects to API and attempted to create an API ticket from parsed IRIS ticket data
+        :param type: abuse type determined from IRIS ticket
+        :param source:  abuse url parsed from IRIS ticket
+        :param report_id: Iris incident number
+        :param reporter_email: reporter email address from iris ticket
+        :param create_date: iris ticket create date
+        :return: returns ticket id on success or empty dict on failure to create ticket
+        """
 
         payload = {'type': type,
                    'source': source,
@@ -22,14 +28,9 @@ class AbuseAPI:
                        'iris_created': create_date.strftime('%Y-%m-%d %H:%M:%S')}}
 
         try:
-            response = requests.post(self._url, json=payload, headers=self.headers)
+            response = requests.post(self._url, json=payload, headers=self._headers)
             response.raise_for_status()
+            return response.content['u_number']
         except Exception as e:
             self._logger.error('Error posting ticket for {} {}'.format(source, e.message))
-
-    def _get_jwt(self):
-        """
-        :return:
-        """
-        # TODO implement mechanism to programmatically retrieve JWT for Abuse API
-        pass
+            return None
