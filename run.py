274790707b7789e1c527ecd5d283850ec1039c12
Nick Wade <nwade@godaddy.com>
2021-05-20 07:30:05 -0500

    Updaet IRIS shim to use the new version of Hermes (#89)

---
+++ run.py
@@ -0,0 +1,29 @@
+import logging
+import os
+from logging.config import dictConfig
+
+import yaml
+
+from iris_shim.handlers import CSAM, Malware, Phishing
+from settings import config_by_name
+
+app_settings = config_by_name[os.getenv('sysenv', 'dev')]()
+
+path = ''
+value = os.getenv('LOG_CFG')
+if value:
+    path = value
+if os.path.exists(path):
+    with open(path, 'rt') as f:
+        lconfig = yaml.safe_load(f.read())
+    dictConfig(lconfig)
+else:
+    logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+if __name__ == '__main__':
+    supported_drivers = [Phishing(app_settings), Malware(app_settings), CSAM(app_settings)]
+
+    for driver in supported_drivers:
+        driver.run()
